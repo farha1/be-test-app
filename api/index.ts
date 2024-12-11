4a3f6371e6c50182abc3a150d1f14c4e440522ce
Farha Ramadhan <farharamadhan0@gmail.com>
2024-12-12 00:43:01 +0700

    add cors

--- api/index.ts
+++ api/index.ts
@@ -2,15 +2,18 @@ import express, { Request, Response } from "express";
 import { getProduct } from "../src/modules/product/productController";
 import { placeOrder } from "../src/modules/order/orderController";
 import bodyParser from "body-parser";
+import cors from "cors";
+
 const app = express();
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Express on Vercell")
+  res.send("Express on Vercell");
 });
 app.get("/products", getProduct);
 app.post("/order", placeOrder);
 
 app.listen(8080, () => console.log("Server ready on port 8080."));
 
-module.exports = app
+module.exports = app;
